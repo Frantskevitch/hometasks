cc1d11556493850946bbc95b23d70b0be9f7a402
root <root@root.com>
2016-08-22 17:09:09 +0300

    hometask 3

--- task2/task4.py
+++ task2/task4.py
@@ -3,5 +3,5 @@ f = open("./access_log")
 for line in f:
       str = line.split(" ")
       stat[str[0]] = stat.get(str[0],0) +1
-c = (sorted(stat, key=stat.__getitem__, reverse=True))
+c = (sorted(stat, key=stat.get, reverse=True))
 print c[:10]
